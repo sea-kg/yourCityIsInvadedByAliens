d568f6426d7e2c437caabfb851d0bcf0833839af
Evgenii Sopov <mrseakg@gmail.com>
2021-01-04 15:05:28 +0700

    Commented rendering textures

--- src/main.cpp
+++ src/main.cpp
@@ -57,19 +57,19 @@ int main(int argc, char* args[]) {
     window.addObject(pFpsText);
 
     // textures
-    window.addObject(new RenderRectTexture(CoordXY(0, 0), grassTexture));
-    window.addObject(new RenderRectTexture(CoordXY(128, 0), grassTexture));
-    window.addObject(new RenderRectTexture(CoordXY(128, 128), grassTexture));
-    window.addObject(new RenderRectTexture(CoordXY(128, 256), grassTexture));
-    window.addObject(new RenderRectTexture(CoordXY(400, 400), grassTexture));
-    window.addObject(new RenderRectTexture(CoordXY(400, 250), grassTexture));
+    // window.addObject(new RenderRectTexture(CoordXY(0, 0), grassTexture));
+    // window.addObject(new RenderRectTexture(CoordXY(128, 0), grassTexture));
+    // window.addObject(new RenderRectTexture(CoordXY(128, 128), grassTexture));
+    // window.addObject(new RenderRectTexture(CoordXY(128, 256), grassTexture));
+    // window.addObject(new RenderRectTexture(CoordXY(400, 400), grassTexture));
+    // window.addObject(new RenderRectTexture(CoordXY(400, 250), grassTexture));
 
     // object
-    window.addObject(new RenderTriangle(
-        CoordXY(320, 200),
-        CoordXY(300, 240),
-        CoordXY(340, 240)
-    ));
+    // window.addObject(new RenderTriangle(
+    //     CoordXY(320, 200),
+    //     CoordXY(300, 240),
+    //     CoordXY(340, 240)
+    // ));
     window.sortObjectsByPositionZ();
 
     bool gameRunning = true;
