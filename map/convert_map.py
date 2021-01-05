d21e1f4936de1057d01ac1ccdc7fff1de27b706b
Evgenii Sopov <mrseakg@gmail.com>
2021-01-05 15:02:14 +0700

    Downloaded tomsk OpenStreetMap

---
+++ map/convert_map.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# munchen
+# https://api.openstreetmap.org/api/0.6/map?bbox=11.54,48.14,11.543,48.145
+
+# tomsk
+# https://api.openstreetmap.org/api/0.6/map?bbox=84.938,56.453,84.993,56.475
